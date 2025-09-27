f0473cbacdf5619cbc4c840e53d336932d7619f0
DiscbotsFRC6023 <discbotsfrc6023@gmail.com>
2025-09-27 15:29:31 -0400

    Wheels moving and in the correct direction

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,12 +4,25 @@
 
 package frc.robot;
 
+import java.util.function.DoubleConsumer;
+
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+
 import edu.wpi.first.util.sendable.SendableRegistry;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+// import com.revrobotics.CANSparkMax;
+// import com.revrobotics.CANSparkLowLevel.MotorType;
+
 
 /**
  * This is a demo program showing the use of the DifferentialDrive class, specifically it contains
@@ -17,32 +30,69 @@ import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
  */
 public class Robot extends TimedRobot {
   private final DifferentialDrive m_robotDrive;
-  private final Joystick m_leftStick;
-  private final Joystick m_rightStick;
-  // private final XboxController m_controller = new XboxController(0);
+  // private final Joystick m_leftStick;
+  // private final Joystick m_rightStick;
+  private final XboxController m_controller = new XboxController(0);
 
-  private final PWMSparkMax m_leftMotor = new PWMSparkMax(3);
-  private final PWMSparkMax m_rightMotor = new PWMSparkMax(1);
+  // private final PWMSparkMax m_leftMotor = new PWMSparkMax(3);
+  // private final PWMSparkMax m_rightMotor = new CANSparkMax(1);
+  private final SparkMax m_leftLeaderMotor = new SparkMax(2, MotorType.kBrushless);
+  private final SparkMax m_rightFollowerMotor = new SparkMax(3, MotorType.kBrushless);
+  private final SparkMax m_rightLeaderMotor = new SparkMax(4, MotorType.kBrushless);
+  private final SparkMax m_leftFollowerMotor = new SparkMax(1, MotorType.kBrushless);
 
   /** Called once at the beginning of the robot program. */
   public Robot() {
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
-    m_rightMotor.setInverted(true);
+    // m_rightMotor.setInverted(true);
 
-    m_robotDrive = new DifferentialDrive(m_leftMotor::set, m_rightMotor::set);
-    m_leftStick = new Joystick(0);
-    m_rightStick = new Joystick(1);
+    // Set up configurations for SparkMax motor controllers:
+    SparkMaxConfig globalConfig = new SparkMaxConfig();
+    SparkMaxConfig rightLeaderConfig = new SparkMaxConfig();
+    SparkMaxConfig leftLeaderConfig = new SparkMaxConfig();
+    SparkMaxConfig rightFollowerConfig = new SparkMaxConfig();
+    SparkMaxConfig leftFollowerConfig = new SparkMaxConfig();
 
-    SendableRegistry.addChild(m_robotDrive, m_leftMotor);
-    SendableRegistry.addChild(m_robotDrive, m_rightMotor);
+    globalConfig
+        .smartCurrentLimit(50)
+        .idleMode(IdleMode.kBrake);
+
+    // Apply the global config and invert since it is on the opposite side
+    rightLeaderConfig
+        .apply(globalConfig)
+        .inverted(true);
+
+    leftLeaderConfig
+        .apply(globalConfig);
+
+    // Apply the global config and set the leader SPARK for follower mode
+    leftFollowerConfig
+        .apply(globalConfig)
+        .follow(m_leftLeaderMotor);
+
+    // Apply the global config and set the leader SPARK for follower mode
+    rightFollowerConfig
+        .apply(globalConfig)
+        .follow(m_rightLeaderMotor);
+
+    m_leftLeaderMotor.configure(leftLeaderConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    m_leftFollowerMotor.configure(leftFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    m_rightLeaderMotor.configure(rightLeaderConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    m_rightFollowerMotor.configure(rightFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+
+    m_robotDrive = new DifferentialDrive(m_leftLeaderMotor::set, m_rightLeaderMotor::set);
+
+    SendableRegistry.addChild(m_robotDrive, m_leftLeaderMotor);
+    SendableRegistry.addChild(m_robotDrive, m_rightLeaderMotor);
   }
 
   @Override
   public void teleopPeriodic() {
-    System.out.println("lefty: " + -m_leftStick.getY() + " righty: " + -m_rightStick.getY() );
-    // m_robotDrive.tankDrive(1.0, 1.0);
-    m_robotDrive.tankDrive(-m_leftStick.getY(), -m_rightStick.getY());
+    // System.out.println("lefty: " + -m_leftStick.getY() + " righty: " + -m_rightStick.getY() );
+    m_robotDrive.tankDrive(m_controller.getLeftY(),m_controller.getRightY());
+    // m_robotDrive.tankDrive(0.5, 0.5);
   }
 }
